8760dea7e2ae67fbc05e813dfcc508963b39e77e
PAULO BELO KAARI FERNANDES <16.00962-2@maua.br>
2019-03-18 11:43:31 -0300

    Projeto criado com Eclipse

---
+++ Pratica02/Olamundoeclipse/src/Olamundoeclipse/MinhaClasseJava.java
@@ -0,0 +1,14 @@
+package Olamundoeclipse;
+
+public class MinhaClasseJava {
+
+	public static void main(String[] args) {
+		int idade = 24;
+		String nome = "Zoio de Gato";
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		
+
+	}
+
+}
